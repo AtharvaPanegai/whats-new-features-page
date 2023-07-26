d0f692a12679fca46d98e4b9defa2567f397c5c1
AtharvaPanegai <panegai.atharv@gmail.com>
2023-07-26 18:24:00 +0530

    adding tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage : {
+        'new-features' : "https://res.cloudinary.com/atharvapanegai/image/upload/v1690482188/Scene_11_1_mbfeto.png"
+      }
+    },
+  },
+  plugins: [],
+}
