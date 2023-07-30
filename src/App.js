5cd3f55c16bf8afa6d368bd58afdd4ff12f3e10a
AtharvaPanegai <panegai.atharv@gmail.com>
2023-07-30 19:20:44 +0530

    to make these pages avaiable only after login

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NewFeaturesComponent from './Components/NewFeaturesComponent';
 import NewFeaturesTwo from './Components/NewFeaturesTwo';
 import WhatsNewUsingPhone from './Components/WhatsNewUsingPhone';
 import { Navigate, Route, Routes } from "react-router-dom";
+import SearchComponent from './Components/SearchComponent';
 
 function App() {
   const [token, setToken] = useState("");
@@ -17,14 +18,24 @@ function App() {
         <Route path='/' element={<NewFeaturesComponent />} />
         <Route path='/two' element={<NewFeaturesTwo />} />
         <Route path='/three' element={<WhatsNewUsingPhone />} />
+        <Route path='/search' element={<SearchComponent />} />
       </Routes>
       {/* )} */}
       {/* {!token && (
+        <Routes>
         <Route path='/' element={<Navigate to="/signin" replace />} />
         <Route path='/signin' element = {<SignInComponent  setToken={setToken} />} />
-      )} */}
+        <Routes />
+        )} */}
     </>
   );
 }
 
 export default App;
+
+// lets store the token into local storage or into a cookie and everytime the app loads 
+// lets check the existance of token using the useToken Hook 
+// it checks the availability of token into local storage and stores into the variable
+// and using that variable we will decide which routes to render
+// if token is not present navigate to the signin page and in signin page use the useToken method passed
+// from app.js to store the value  
