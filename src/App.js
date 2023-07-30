a27a6e040c26e894697a2158c7100b3236f755b5
AtharvaPanegai <panegai.atharv@gmail.com>
2023-07-30 19:00:00 +0530

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 
+import { useState } from 'react';
 import './App.css';
 import NewFeaturesComponent from './Components/NewFeaturesComponent';
 import NewFeaturesTwo from './Components/NewFeaturesTwo';
 import WhatsNewUsingPhone from './Components/WhatsNewUsingPhone';
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
+  const [token, setToken] = useState("");
   return (
-    <div className="App">
-      {/* <NewFeaturesComponent /> */}
-      {/* <NewFeaturesTwo /> */}
-      <WhatsNewUsingPhone />
-    </div>
+
+    <>
+      {/* {token && ( */}
+
+      <Routes>
+        <Route path='/' element={<NewFeaturesComponent />} />
+        <Route path='/two' element={<NewFeaturesTwo />} />
+        <Route path='/three' element={<WhatsNewUsingPhone />} />
+      </Routes>
+      {/* )} */}
+      {/* {!token && (
+        <Route path='/' element={<Navigate to="/signin" replace />} />
+        <Route path='/signin' element = {<SignInComponent  setToken={setToken} />} />
+      )} */}
+    </>
   );
 }
 
